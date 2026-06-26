27f6e0e0738d7b0869ffab2dde64da335b8f0c86
hsiv24 <vishalsaini24june@gmail.com>
2026-06-26 19:52:15 +0530

    chapter-2 question-5

---
+++ que5.js
@@ -0,0 +1,7 @@
+// Write your code below:
+let fruits = document.getElementsByTagName("li");
+
+fruits[4].style.color = "red";
+for (let i = 0; i < fruits.length; i++){
+    fruits[i].style.fontStyle = "italic"
+}
